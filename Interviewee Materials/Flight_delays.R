bd56f5cb349d16538852b6a6ad50ac27d78d0210
sushmit <sroy@nyc-007152-mbp.local>
2016-07-25 03:41:38 -0400

    adding

--- Interviewee Materials/Flight_delays.R
+++ Interviewee Materials/Flight_delays.R
@@ -1,4 +1,5 @@
-library(ggplot2)
+library(MASS)
+set.seed(1)
 # Reading the data
 CancelledFlights <- read.csv(file="CancelledFlights.csv",head=FALSE,sep=",")
 # adding column names
@@ -18,3 +19,65 @@ cancel_month <- table(Canceled,Month)
 barplot(cancel_month, col=c("darkblue","red"),legend = rownames(cancel_month))
 cancel_unique_carrier <- table(Canceled,UniqueCarrier)
 barplot(cancel_unique_carrier, col=c("darkblue","red"),legend = rownames(cancel_unique_carrier))
+
+error_vec = rep(0,nrow(CancelledFlights))
+for (i in 1:dim(CancelledFlights)[1] ){
+  glm.fit = glm(Canceled ~ ., data =CancelledFlights[-i,],family = binomial)
+  pos_prob = predict(glm.fit, CancelledFlights[i,],type = "response")
+  if(pos_prob > 0.5)
+  {
+    predicted_value = 1
+  }else{
+    predicted_value = 0
+  }
+  if(predicted_value!=CancelledFlights[i,]$Canceled) {
+    error_vec[i] = 1
+  }
+}
+
+print('The Logistic MSE is : ')
+print(mean(error_vec))
+### lda
+error_vec = rep(0,nrow(CancelledFlights))
+for (i in 1:dim(CancelledFlights)[1] ){
+  lda.fit = lda(Canceled ~ ., data =CancelledFlights[-i,])
+  pos_prob =predict(lda.fit, CancelledFlights[i, ])$posterior[,1]
+  if(pos_prob < 0.5)
+  {
+    predicted_value = 1
+  }else{
+    predicted_value = 0
+  }
+  if(predicted_value!=CancelledFlights[i,]$Canceled) {
+    error_vec[i] = 1
+  }
+}
+print('The LDA MSE is : ')
+print(mean(error_vec))
+
+
+summary(glm.fit)
+
+# Input a dataframe with same structure 
+predict.flightdelays = function(CancelledFlights) {
+  for (i in 1:dim(CancelledFlights)[1]) {
+    pos_prob = predict(lda.fit, CancelledFlights[i,])$posterior[, 1]
+  }
+  
+  if (pos_prob < 0.5)
+  {
+    print(" There is a high probability of Cancellation")
+  } else{
+    print(" There is a low probability of Cancellation")
+  }
+  
+}
+  
+
+
+
+
+
+
+
+
