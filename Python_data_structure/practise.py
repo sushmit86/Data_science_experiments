83d9b7722ca5f4c8429c91573ac47eefdbaa1be9
SUSHMIT ROY <sushmitroy@SUSHMITs-MacBook-Pro.local>
2016-08-24 01:20:12 -0400

    adding

--- Python_data_structure/practise.py
+++ Python_data_structure/practise.py
@@ -1,18 +1,57 @@
 from decimal import Decimal
+
 print((999).bit_length())
 ####
 s = 11
 d = int(s)
 print(d)
-sum (Decimal ("0.1") for i in range(10)) == Decimal("1.0")
+sum(Decimal("0.1") for i in range(10)) == Decimal("1.0")
+
+
 ###
-def convert_to_decimal(number,base):
-    multiplier, result = 1 , 0
+def convert_to_decimal(number, base):
+    multiplier, result = 1, 0
     while number > 0:
-        result += number%10*multiplier
+        result += number % 10 * multiplier
         multiplier *= base
-        number = number//10
+        number //= 10
     return result
+
+
 def test_convert_to_decimal():
-    number , base = 1001,2
-    assert(convert_to_decimal(number, base)==9)
+    number, base = 1001, 2
+    assert (convert_to_decimal(number, base) == 9)
+
+
+def convert_from_decimal(number, base):
+    multiplier, result = 1, 0
+    while number > 0:
+        result += number % base * multiplier
+        multiplier *= 10
+        number //= 10
+    return result
+
+
+def test_convert_from_decimal():
+    number, base = 9, 2
+    assert (convert_from_decimal(number, base) == 1001)
+
+
+def convert_from_decimal_larger_bases(number, base):
+    strings = "0123456789ABCDEFGHIJ"
+    result = ""
+    while number > 0:
+        digits = number % base
+        result = strings[digits] + result
+        number //= base
+    return result
+
+def test_convert_from_decimal_larger_bases():
+    number, base = 31, 16
+    assert (convert_from_decimal_larger_bases(number,base) == '1F')
+    
+def convert_dec_to_any_base_rec(number, base):
+    convertstring = '0123456789ABCDEFGHIJ'
+    if number < base : return convertstring[number]
+    else:
+        return convert_dec_to_any_base_rec(number//base) + convert_dec_to_any_base_rec(number % base)
