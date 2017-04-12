59d081dd521ac9ac9be37b8fa72099fe70a1fea7
SUSHMIT ROY <sushmitroy@SUSHMITs-MacBook-Pro.local>
2017-04-11 23:37:57 -0400

    adding

--- R-Marketing/examples.R
+++ R-Marketing/examples.R
@@ -4,3 +4,26 @@ head(satData)
 summary(satData)
 library(corrplot)
 corrplot.mixed(cor(satData[,-3]))
+aggregate(iProdSAT ~ Segment, satData,mean)
+sat.anova  <- aov(iProdSAT ~ -1 + Segment,satData)
+summary(sat.anova)
+library(multcomp)
+par(mar = c(4,8,4,2))
+plot(glht(sat.anova))
+seq(from = -5,to = 28, by =4)
+rep(c(1,2,3),each =4)
+xSeq =1:10
+xSeq[-5:-7]
+xNum = 10:20
+xSub <- xNum[2:4]
+xNum = c(1.0,3.14,5.0,7.0)
+xNum
+xNum[c(FALSE, TRUE, TRUE,TRUE)]
+xNum[xNum > 5]
+str(xNum)
+xChar <- c("foo","bar","boo","far")
+xList <- list(xNum, xChar)
+xList[1]
+xChar <- c("foo","bar","boo","far")
+str(xChar)
+list.files()
