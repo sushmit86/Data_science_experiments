8eea140c5c1a457205c24180b7b3b01af276051c
SUSHMIT ROY <sushmitroy@SUSHMITs-MacBook-Pro.local>
2017-06-06 00:48:18 -0400

    adding exercise 7

--- DBDA2Eprograms/DBDA2E_utilities.py
+++ DBDA2Eprograms/DBDA2E_utilities.py
@@ -14,67 +14,6 @@ import scipy as sc
 import pymc3
 
 
-JS_CODE = """
-import {Label, LabelView} from "models/annotations/label"
-
-export class LatexLabelView extends LabelView
-  render: () ->
-
-    #--- Start of copied section from ``Label.render`` implementation
-
-    ctx = @plot_view.canvas_view.ctx
-
-    # Here because AngleSpec does units tranform and label doesn't support specs
-    switch @model.angle_units
-      when "rad" then angle = -1 * @model.angle
-      when "deg" then angle = -1 * @model.angle * Math.PI/180.0
-
-    if @model.x_units == "data"
-      vx = @xmapper.map_to_target(@model.x)
-    else
-      vx = @model.x
-    sx = @canvas.vx_to_sx(vx)
-
-    if @model.y_units == "data"
-      vy = @ymapper.map_to_target(@model.y)
-    else
-      vy = @model.y
-    sy = @canvas.vy_to_sy(vy)
-
-    if @model.panel?
-      panel_offset = @_get_panel_offset()
-      sx += panel_offset.x
-      sy += panel_offset.y
-
-    #--- End of copied section from ``Label.render`` implementation
-
-    # Must render as superpositioned div (not on canvas) so that KaTex
-    # css can properly style the text
-    @_css_text(ctx, "", sx + @model.x_offset, sy - @model.y_offset, angle)
-
-    # ``katex`` is loaded into the global window at runtime
-    # katex.renderToString returns a html ``span`` element
-    katex.render(@model.text, @el, {displayMode: true})
-
-export class LatexLabel extends Label
-  type: 'LatexLabel'
-  default_view: LatexLabelView
-"""
-
-class LatexLabel(Label):
-    """A subclass of the Bokeh built-in `Label` that supports rendering
-    LaTex using the KaTex typesetting library.
-
-    Only the render method of LabelView is overloaded to perform the
-    text -> latex (via katex) conversion. Note: ``render_mode="canvas``
-    isn't supported and certain DOM manipulation happens in the Label
-    superclass implementation that requires explicitly setting
-    `render_mode='css'`).
-    """
-    __javascript__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"]
-    __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
-    __implementation__ = JS_CODE
-
 
 
 
@@ -123,34 +62,26 @@ def plotPost(paramSampleVec, cenTend = 'mode', col = None, showCurve=False ,titl
                  border_line_color='white', border_line_alpha=1.0,
                  background_fill_color='white', background_fill_alpha=1.0)
     labels = LabelSet(x='HDI_x', y='HDI_y', text='HDI_x', level='glyph', source=source, render_mode='canvas')
+    HDI_label = Label(x=np.mean(HDI_x), y= 2*np.mean(HDI_y), x_units='data', y_units='data',
+             text='HDI', render_mode='css',
+             border_line_color='white', border_line_alpha=1.0,
+             background_fill_color='white', background_fill_alpha=1.0)
+
     p1.add_layout(mode_label)
     p1.add_layout(labels)
+    p1.add_layout(HDI_label)
     p1.xaxis.axis_label = xlab
     p1.xaxis.minor_tick_line_color = None
     p1.xaxis.bounds = (0, 1)
     p1.ygrid.visible = False
     p1.yaxis.visible = False
     p1.xgrid.visible = False
-
+    p1.title.align = 'center'
     return p1
 
 
 
-def func_latex():
-    x = np.arange(0.0, 1.0 + 0.01, 0.01)
-    y = np.cos(2*2*np.pi*x) + 2
 
-    p = figure(title="LaTex Demonstration",plot_width=500, plot_height=500)
-    p.line(x, y)
-
-    # Note: must set ``render_mode="css"``
-    latex = LatexLabel(text="",
-                   x=0.5, y=2,
-                    text_font_size='10pt',render_mode='css',
-                   background_fill_color='#ffffff')
-
-    #p.add_layout(latex)
-    return(p)
 
 
 
