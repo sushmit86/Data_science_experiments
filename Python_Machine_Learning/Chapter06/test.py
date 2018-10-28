bdaddba6d0d15d379ec6e1f4e312d3777f0a7430
sushmit-staples <Sushmit.Roy@Staples.com>
2018-10-28 18:30:01 -0400

    adding

---
+++ Python_Machine_Learning/Chapter06/test.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Reading data
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+#%%
+print(3)
+
