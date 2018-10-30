22d394731a5db031513824591203d5bdc923f01a
sushmit-staples <Sushmit.Roy@Staples.com>
2018-10-30 15:47:15 -0400

    adding

--- Python_Machine_Learning/Chapter06/test.py
+++ Python_Machine_Learning/Chapter06/test.py
@@ -4,14 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.piple
 
 ## Reading data
 # %%
 df = pd.read_csv('wdbc.data',header=None)
 df.head()
 
+## label encoding first
+
 # %%
 le = preprocessing.LabelEncoder()
 X = df.iloc[:,2:].values
-Y = df.iloc[:,1].values
-le.fit_transform(Y)
+y = df.iloc[:,1].values
+y=le.fit_transform(y)
+le.classes_
+# split the data test train_test_split
+X_train,y_train,X_test,y_test = train_test_split(X,y,test_size=0.20,random_state=1)
