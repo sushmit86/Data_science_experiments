df10f0144fd4c107166d500a0d2a0a21af760434
Roy <sushmit.roy@staples.com>
2019-02-12 15:21:03 -0500

    adding

--- dask_exprimentations/test_file.py
+++ dask_exprimentations/test_file.py
@@ -1,4 +1,21 @@
-print('This is test')
-print('Another test')
- test
- 
+import time
+import ray
+ray.init()
+def f1():
+    time.sleep(1)
+
+@ray.remote
+def f2():
+    time.sleep(1)
+start_time = time.time()
+# The following takes ten seconds.
+[f1() for _ in range(10)]
+elapsed_time = time.time() - start_time
+print('Time taken for step 1',elapsed_time)
+
+# The following takes one second (assuming the system has at least ten CPUs).
+start_time = time.time()
+ray.get([f2.remote() for _ in range(10)])
+elapsed_time = time.time() - start_time
+print('Time taken for step 2',elapsed_time)
+ray.shutdown()
